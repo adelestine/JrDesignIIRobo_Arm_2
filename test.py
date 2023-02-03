5a999ad418307e423ae227d8bdff69c5f5d9d4a7
Astrid Delestine <delestic@oregonstate.edu>
2023-02-02 19:19:45 -0800

    edit lol

--- test.py
+++ test.py
@@ -4,12 +4,22 @@ from pyfirmata import Arduino, util
 board = pyfirmata.Arduino('COM5')
 
 while True:
+    board.digital[13].write(1)
+    time.sleep(1)
+    board.digital[13].write(0)
+    time.sleep(4)
+
+
+
+
+# while True:
     
-    board.digitalWrite(LED_BUILTIN, HIGH)
-    board.delay(1000)
-    board.digitalWrite(LED_BUILTIN, LOW)
-    board.delay(1000);
-    # board.digital[13].write(1)
-    # time.sleep(1)
-    # board.digital[13].write(0)
-    # time.sleep(1)p
+#     board.pinMode(LED_BUILTIN, OUTPUT)
+#     board.digitalWrite(LED_BUILTIN, HIGH)
+#     board.delay(1000)
+#     board.digitalWrite(LED_BUILTIN, LOW)
+#     board.delay(1000);
+#     # board.digital[13].write(1)
+#     # time.sleep(1)
+#     # board.digital[13].write(0)
+#     # time.sleep(1)p
